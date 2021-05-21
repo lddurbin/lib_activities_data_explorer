9bf64c2983fcb998a006b9f8965c3d9db0a03d5b
lddurbin <l.d.durbin@gmail.com>
2021-05-21 13:06:32 +1200

    Remove Local Board names.

--- data_prep.R
+++ data_prep.R
@@ -5,18 +5,18 @@ library("fs")
 
 files <- dir_ls("data", regexp = "\\.xlsx$")
 
-# Remove submission time, yes/no questions, morning/afternoon/evening, CC staff type.
+# Remove submission time, yes/no questions, morning/afternoon/evening, CC staff type, LB names.
 df <- files %>% 
   map_dfr(read_excel) %>% 
-  select(-c(4, 9:10, 15, 49, 72, 74, 76, 78, 80)) %>% 
+  select(-c(4, 9:10, 15, 20, 49:50, 72, 74, 76, 78, 80)) %>% 
   clean_names() %>% 
   mutate(id = as.character(id))
 
 # online_session, offline_session, adults_offline, children_offline, total_offline, adults_online, children_online, total_online, session_datetime, session_location
 
 delivery_agents <- df %>% 
-  select(1, 41:43, 45:66) %>%
-  select(id, agent_types = 2, external_agent_names = 3, AC_agent_names = 4, local_board_names = 5, unit_names = 22, unit_1_teams = 23, unit_2_teams = 24, unit_3_teams = 25, unit_4_teams = 26, everything()) %>% 
+  select(1, 40:42, 44:64) %>%
+  select(id, agent_types = 2, external_agent_names = 3, AC_agent_names = 4, unit_names = 21, unit_1_teams = 22, unit_2_teams = 23, unit_3_teams = 24, unit_4_teams = 25, everything()) %>% 
   pivot_longer(11:length(.), values_to = "library_names", values_drop_na = TRUE) %>% 
   mutate(across(c("agent_types", "local_board_names", "library_names"), ~str_sub(.x, 2, -2))) %>% 
   separate_rows(c("agent_types", "local_board_names", "library_names"), sep = ",") %>% 
