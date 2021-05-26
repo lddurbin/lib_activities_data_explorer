65efcf022211f6dcba028a5b084ccae9d1a8d23a
lddurbin <l.d.durbin@gmail.com>
2021-05-26 16:17:07 +1200

    Move datetime col creation into df_2 dataframe.

--- data_prep.R
+++ data_prep.R
@@ -15,7 +15,7 @@ df_1 <- files %>%
   clean_names() %>% 
   mutate(id = as.character(id))
 
-# Online/offline columns, concatenate locations into 1 column
+# Online/offline columns, concatenate locations into 1 column, datetime column
 df_2 <- df_1 %>% 
   select(-c(41:43, 45:66)) %>%
   mutate(
@@ -25,18 +25,16 @@ df_2 <- df_1 %>%
     ) %>% 
   unite("location", starts_with("where_in"), na.rm = TRUE, remove = TRUE) %>% 
   unite("sublocation", c(17:19), na.rm = TRUE, remove = TRUE) %>% 
-  unite("delivery_time", starts_with("at_what_time"), na.rm = TRUE, remove = TRUE)
-
-df_3 <- df_2 %>% 
+  unite("delivery_time", starts_with("at_what_time"), na.rm = TRUE, remove = TRUE) %>% 
   mutate(delivery_datetime = parse_date_time(paste(when_was_the_session_delivered, delivery_time), c("Ymd HMp", "Ymd Hp")), .keep = "unused")
-
-source("delivery_agents.R")
 
 age_groups <- multichoice_splitting(df_1, what_was_the_target_age_group_for_this_session, age_group)
 
 target_groups <- multichoice_splitting(df_1, which_of_these_groups_was_the_session_designed_to_benefit, target_group)
 
 realm_languages <- multichoice_splitting(df_1, which_language_s_was_the_session_delivered_in, realm_language)
+
+source("delivery_agents.R")
 
 # df_x <- df_y %>% 
 #   left_join(delivery_agents, by = "id")
