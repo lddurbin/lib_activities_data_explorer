960801831b19e782486165fa7cb15ef280e65b2b
Lee Durbin <l.d.durbin@gmail.com>
2021-05-31 22:59:40 +0000

    Create one column to show all Conncted Communities delivery agents

--- functions.R
+++ functions.R
@@ -7,3 +7,11 @@ multichoice_splitting <- function(data, multichoice_col, new_col) {
     mutate({{new_col}} := str_replace_all({{new_col}}, '"', '')) %>% 
     filter(!is.na({{new_col}}))
 }
+
+# Concatenate multiple rows within each group into a single cell per group
+concat_rows <- function(data, col_to_concat) {
+  data %>% 
+    group_by(id) %>% 
+    distinct({{col_to_concat}}) %>% 
+    summarise({{col_to_concat}} := str_c({{col_to_concat}}, collapse = " | "), .groups = "drop")
+}
