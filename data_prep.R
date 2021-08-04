b80ae982ac51ceaf3c53518300a072898062a5d7
lddurbin <l.d.durbin@gmail.com>
2021-08-05 10:29:59 +1200

    Format question: group free-text responses as "Other"

--- data_prep.R
+++ data_prep.R
@@ -21,13 +21,21 @@ df <- files %>%
   filter(email != "durbinl@aklc.govt.nz")
 
 # Online/offline columns, concatenate locations into 1 column, datetime column
+# CONVERT DURATION INTO TIME
 base_table <- df %>% 
   select(-c(42:44, 46:70, 72:77)) %>%
   mutate(in_person = str_detect(how_was_the_session_delivered, "person"), online = str_detect(how_was_the_session_delivered, "Online"), .keep = "unused") %>% 
   unite("location", starts_with("where_in"), na.rm = TRUE, remove = TRUE) %>% 
   unite("sublocation", c(18:20), na.rm = TRUE, remove = TRUE) %>% 
   unite("delivery_time", starts_with("at_what_time"), na.rm = TRUE, remove = TRUE) %>% 
-  mutate(delivery_datetime = parse_date_time(paste(when_was_the_session_delivered, delivery_time), c("Ymd HMp", "Ymd Hp")), .keep = "unused") %>%
+  mutate(
+    delivery_datetime = parse_date_time(paste(when_was_the_session_delivered, delivery_time), c("Ymd HMp", "Ymd Hp")),
+    what_was_the_format_of_the_session = case_when(
+      !what_was_the_format_of_the_session %in% unlist(c("Book a Librarian", "Class or workshop", "Community event", "Club", "Live performance", "Pre-school activity", "Promotion or roadshow", "Talk")) ~ "Other",
+      !is.na(what_was_the_format_of_the_session) ~ what_was_the_format_of_the_session
+    ),
+    .keep = "unused"
+    ) %>%
   rowwise() %>% 
   mutate(location = case_when(
     str_length(sublocation) < 2 ~ location,
