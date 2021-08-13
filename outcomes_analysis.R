db9604884c7810b0e02b3746cc42ff8adae38bf7
lddurbin <l.d.durbin@gmail.com>
2021-08-13 12:43:34 +1200

    Progress Outcomes report. Create function for creating all 6 reports at once, export to Reports folder.

--- outcomes_analysis.R
+++ outcomes_analysis.R
@@ -95,13 +95,26 @@ outcome_data %>%
   filter(!is.na(what_was_the_format_of_the_session)) %>% 
   summarise_data(what_was_the_format_of_the_session)
 
-# remove(list = ls())
-# 
-# rmarkdown::render("outcomes_analysis.Rmd", params = list(
-#   year = 2021,
-#   month = "July",
-#   outcome = "Environment",
-#   outcome_owner = "Kirstin Kane",
-#   outcome_url = "https://aklcouncil.sharepoint.com/sites/tools-to-do-my-job/SitePages/Literacy-@-Auckland-Libraries.aspx"
-# ),
-# envir = new.env())
+
+# Knit file based on chosen outcome
+render_analysis <- function(outcome_data, month, year) {
+  rmarkdown::render("outcomes_analysis.Rmd", output_file = paste0("Reports/", outcome_data[4]), params = list(
+    year = year,
+    month = month,
+    outcome = outcome_data[1],
+    outcome_owner = outcome_data[2],
+    outcome_url = outcome_data[3]
+  ),
+  envir = new.env())
+}
+
+kia_ora <- c("Kia ora Tāmaki Makaurau", " and Judith Waaka", "", "kia_ora")
+literacy <- c("Literacy", " and Kirstin Kane", "https://aklcouncil.sharepoint.com/sites/tools-to-do-my-job/SitePages/Literacy-@-Auckland-Libraries.aspx", "literacy")
+talanoa <- c("Talanoa", " and Elenoa Mo'a Sili-Mati", "", "talanoa")
+belonging <- c("Tātou / Belonging", " and Megan Grimshaw-Jones", "", "belonging")
+heritage <- c("Heritage", ", Stacey Smith, and  Jacqueline Snee", "", "heritage")
+environment <- c("Environment", "", "", "environment")
+
+#walk(list(kia_ora, literacy, talanoa, belonging, heritage, environment), render_analysis, month = "July", year = 2021)
+
+render_analysis(literacy, month = "July", year = 2021)
