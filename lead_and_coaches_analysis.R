e749a45637a418ea4196ec1b41d42a3dff881c51
lddurbin <l.d.durbin@gmail.com>
2021-08-23 20:57:14 +1200

    Continue Local Boards analysis

--- lead_and_coaches_analysis.R
+++ lead_and_coaches_analysis.R
@@ -4,10 +4,14 @@ source("data_prep.R")
 
 local_board_data <- read_csv("data/local_board_groups.csv", col_types = "cdcc")
 
-group_and_sum <- function(sum_name, sum_col) {
-  local_board_overall %>% 
-    group_by(local_board = in_which_local_board_was_the_session_delivered) %>% 
-    summarise({{sum_name}} := sum({{sum_col}}, na.rm = TRUE), .groups = "drop")
+group_and_sum <- function(df, group_name, group_col) {
+  df %>% 
+    group_by({{group_name}} := {{group_col}}) %>% 
+    summarise(
+      total_hours = round(sum(what_was_the_duration_of_the_session_to_the_nearest_half_an_hour, na.rm = TRUE)/60),
+      adult_participants = sum(across(starts_with(c("how_many_adults", "how_many_people"))), na.rm = TRUE),
+      child_participants = sum(across(starts_with(c("how_many_children", "how_many_people"))), na.rm = TRUE),
+      .groups = "drop")
 }
 
 get_local_board_data <- function(df) {
@@ -19,29 +23,43 @@ get_local_board_data <- function(df) {
     ))
 }
 
-monthly_submissions <- joined_data %>% 
-  filter(month(as_date(delivery_datetime), label = TRUE, abbr = FALSE) == "July" & year(as_date(delivery_datetime)) == 2021) %>% 
+
+# Total submissions -------------------------------------------------------
+
+monthly_submissions_programmes_and_events <- joined_data %>% 
+  filter(month(as_date(delivery_datetime), label = TRUE, abbr = FALSE) == "July" & year(as_date(delivery_datetime)) == 2021 & what_was_the_format_of_the_session != "Book a Librarian") %>% 
   distinct(id) %>% 
   nrow()
 
+monthly_submissions_book_a_librarian <- joined_data %>% 
+  filter(month(as_date(delivery_datetime), label = TRUE, abbr = FALSE) == "July" & year(as_date(delivery_datetime)) == 2021 & what_was_the_format_of_the_session == "Book a Librarian") %>% 
+  distinct(id) %>% 
+  nrow()
+
+
+# Local Board data --------------------------------------------------------
+
 local_board_overall <- joined_data %>% 
-  filter(!is.na(in_which_local_board_was_the_session_delivered) & month(as_date(delivery_datetime), label = TRUE, abbr = FALSE) == "July" & year(as_date(delivery_datetime)) == 2021) %>% 
+  filter(!is.na(in_which_local_board_was_the_session_delivered) & month(as_date(delivery_datetime), label = TRUE, abbr = FALSE) == "July" & year(as_date(delivery_datetime)) == 2021 & what_was_the_format_of_the_session != "Book a Librarian") %>% 
   distinct(id, .keep_all = TRUE)
 
-# Of these, how many were delivered in each Local Board?
+local_board_highlighted <- local_board_overall %>% 
+  rename(local_board = in_which_local_board_was_the_session_delivered) %>% 
+  get_local_board_data() %>% 
+  filter(highlight_local_board == TRUE)
+
+
+# Stats by Local Board ----------------------------------------------------
+
 local_board_submissions <- local_board_overall %>% 
   count(local_board = in_which_local_board_was_the_session_delivered, name = "sessions", sort = TRUE) 
 
-# For each Local Board, show the total hours of delivery, adult participants, and child participants
-local_board_summary <- group_and_sum(total_hours, what_was_the_duration_of_the_session_to_the_nearest_half_an_hour) %>% 
-  left_join(group_and_sum(adult_participants, across(starts_with(c("how_many_adults", "how_many_people"))))) %>% 
-  left_join(group_and_sum(child_participants, across(starts_with(c("how_many_children", "how_many_people"))))) %>% 
-  left_join(local_board_submissions) %>% 
+local_board_summary <- group_and_sum(local_board_overall, local_board, in_which_local_board_was_the_session_delivered) %>% 
+  left_join(local_board_submissions, by = "local_board") %>% 
   get_local_board_data() %>% 
   pivot_longer(cols = 2:5, names_to = "metric") %>% 
   mutate(metric = str_replace(metric, "_", " ") %>% str_to_title() %>% factor(levels = c("Sessions", "Total Hours", "Adult Participants", "Child Participants")))
 
-# For each Local Board, show total sessions, hours of delivery, and participants (adults and children), highlighting current Local Board
 local_board_summary %>% 
   ggplot(mapping = aes(x = tidytext::reorder_within(local_board, value, metric), y = value, fill = highlight_local_board)) +
   geom_col() +
@@ -50,7 +68,70 @@ local_board_summary %>%
   scale_fill_brewer(palette="Dark2") +
   geom_text(aes(label = prettyNum(value, big.mark = ",")), hjust = 1.2, colour = "white") +
   theme(legend.position = "none", axis.title = element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank()) +
-  scale_y_continuous(expand = c(0,0)) +
+  coord_flip()
+
+
+# Stats by locations -------------------------------------------------
+
+location_submissions <- local_board_highlighted %>%
+  count(location, name = "sessions", sort = TRUE) 
+
+location_summary <- group_and_sum(local_board_highlighted, location, location) %>% 
+  left_join(location_submissions, by = "location") %>% 
+  pivot_longer(cols = 2:5, names_to = "metric") %>% 
+  mutate(metric = str_replace(metric, "_", " ") %>% str_to_title() %>% factor(levels = c("Sessions", "Total Hours", "Adult Participants", "Child Participants")))
+
+location_summary %>% 
+  ggplot(mapping = aes(x = tidytext::reorder_within(location, value, metric), y = value)) +
+  geom_col(fill = "blue") +
+  tidytext::scale_x_reordered() +
+  facet_wrap("metric", scales = "free") +
+  geom_text(aes(label = prettyNum(value, big.mark = ",")), hjust = 1.2, colour = "white") +
+  theme(legend.position = "none", axis.title = element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank()) +
+  coord_flip()
+
+
+# Stats by session format -------------------------------------------------
+
+session_format_submissions <- local_board_highlighted %>%
+  count(session_format = what_was_the_format_of_the_session, name = "sessions", sort = TRUE) 
+
+session_format_summary <- group_and_sum(local_board_highlighted, session_format, what_was_the_format_of_the_session) %>% 
+  left_join(session_format_submissions, by = "session_format") %>% 
+  pivot_longer(cols = 2:5, names_to = "metric") %>% 
+  mutate(metric = str_replace(metric, "_", " ") %>% str_to_title() %>% factor(levels = c("Sessions", "Total Hours", "Adult Participants", "Child Participants")))
+
+session_format_summary %>% 
+  ggplot(mapping = aes(x = tidytext::reorder_within(session_format, value, metric), y = value)) +
+  geom_col(fill = "blue") +
+  tidytext::scale_x_reordered() +
+  facet_wrap("metric", scales = "free") +
+  geom_text(aes(label = prettyNum(value, big.mark = ",")), hjust = 1.2, colour = "white") +
+  theme(legend.position = "none", axis.title = element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank()) +
+  coord_flip()
+
+
+# Stats by outcomes -------------------------------------------------
+
+outcome_submissions <- local_board_highlighted %>%
+  count(outcome, name = "sessions", sort = TRUE) 
+
+outcome_summary <- group_and_sum(local_board_highlighted, outcome, outcome) %>% 
+  left_join(outcome_submissions, by = "outcome") %>% 
+  pivot_longer(cols = 2:5, names_to = "metric") %>% 
+  mutate(outcome = case_when(
+    is.na(outcome) ~ "None",
+    !is.na(outcome) ~ outcome
+  )) %>% 
+  mutate(metric = str_replace(metric, "_", " ") %>% str_to_title() %>% factor(levels = c("Sessions", "Total Hours", "Adult Participants", "Child Participants")))
+
+outcome_summary %>% 
+  ggplot(mapping = aes(x = tidytext::reorder_within(outcome, value, metric), y = value)) +
+  geom_col(fill = "blue") +
+  tidytext::scale_x_reordered() +
+  facet_wrap("metric", scales = "free") +
+  geom_text(aes(label = prettyNum(value, big.mark = ",")), hjust = 1.2, colour = "white") +
+  theme(legend.position = "none", axis.title = element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank()) +
   coord_flip()
 
 
