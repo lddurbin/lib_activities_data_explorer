1f1702d641e759b7ce777530afb1f43077c7f4a7
lddurbin <l.d.durbin@gmail.com>
2021-08-28 08:28:47 +1200

    Include participants total in mini explorer. Fix URLs to other reports.

--- outcomes_analysis.R
+++ outcomes_analysis.R
@@ -6,17 +6,18 @@ render_analysis <- function(outcome_data, month, year) {
     outcome = outcome_data[1],
     outcome_owner = outcome_data[2],
     outcome_url = outcome_data[3],
-    outcome_filename = outcome_data[4]
+    outcome_filename = outcome_data[4],
+    outcome_file_location = outcome_data[5]
   ),
   envir = new.env())
 }
 
-kia_ora <- c("Kia ora Tāmaki Makaurau", " for Judith Waaka", "https://aklcouncil.sharepoint.com/sites/who-we-are/SitePages/maori-outcomes.aspx", "kia_ora")
-literacy <- c("Literacy", " for Kirstin Kane", "https://aklcouncil.sharepoint.com/sites/tools-to-do-my-job/SitePages/Literacy-@-Auckland-Libraries.aspx", "literacy")
-talanoa <- c("Talanoa", " for Elenoa Mo'a Sili-Mati", "https://aklcouncil.sharepoint.com/sites/who-we-are/SitePages/Pacific-Framework.aspx", "talanoa")
-belonging <- c("Tātou / Belonging", " for Megan Grimshaw-Jones", "", "belonging")
-heritage <- c("Heritage", ", for Stacey Smith, and  Jacqueline Snee", "https://aklcouncil.sharepoint.com/sites/teams-groups/SitePages/heritage-team.aspx", "heritage")
-environment <- c("Environment", "", "", "environment")
+kia_ora <- c("Kia ora Tāmaki Makaurau", " for Judith Waaka", "https://aklcouncil.sharepoint.com/sites/who-we-are/SitePages/maori-outcomes.aspx", "kia_ora", Sys.getenv("KIAORA_URL"))
+literacy <- c("Literacy", " for Kirstin Kane", "https://aklcouncil.sharepoint.com/sites/tools-to-do-my-job/SitePages/Literacy-@-Auckland-Libraries.aspx", "literacy", Sys.getenv("LITERACY_URL"))
+talanoa <- c("Talanoa", " for Elenoa Mo'a Sili-Mati", "https://aklcouncil.sharepoint.com/sites/who-we-are/SitePages/Pacific-Framework.aspx", "talanoa", Sys.getenv("TALANOA_URL"))
+belonging <- c("Tātou / Belonging", " for Megan Grimshaw-Jones", "", "belonging", Sys.getenv("BELONGING_URL"))
+heritage <- c("Heritage", ", for Stacey Smith, and  Jacqueline Snee", "https://aklcouncil.sharepoint.com/sites/teams-groups/SitePages/heritage-team.aspx", "heritage", Sys.getenv("HERITAGE_URL"))
+environment <- c("Environment", "", "", "environment", Sys.getenv("ENVIRONMENT_URL"))
 
 all_outcomes <- list(kia_ora, literacy, talanoa, belonging, heritage, environment)
 
